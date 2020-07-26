bb65b69f751b4b9010efb7f4a2712d0e236dee07
Veber <andrei.veber@capgemini.com>
2020-07-26 18:00:54 +0200

    #2 Added MetadataFunctions. Code style refactoring

--- Constants.js
+++ Constants.js
@@ -1,16 +1,25 @@
-// Project constants
-var SUMMARY_BALANCE_SHEET_NAME = "Summary Balance";
-var RAW_DATA_SHEET_NAME = "Raw Data";
-var TRANSACTIONS_HISTORY = "Transactions History";
-var IS_TRANSACTION_PLANNED = "Is planned";
-var CELL_BACKGROUND_COLOR = {
+/**
+ * Global constants
+ */
+
+const Sheets = {
+  SUMMARY_BALANCE:      "Summary Balance",
+  RAW_DATA:             "Raw Data",
+  TRANSACTIONS_HISTORY: "Transactions History",
+  METADATA:             "Metadata"
+}
+
+const IS_TRANSACTION_PLANNED = "Is planned";
+
+const CELL_BACKGROUND_COLOR = {
   spent:       "#c9daf8",
   almostSpent: "#d9ead3",
   notSpent:    "#e6b8af",
   noFill:      "#ffffff"
 };
-var PLANNED_PAYMENT_WARNING_TITLE = "Необходимо проверить список транзакций!";
-var DAILY_EXPENSES_DATA_TITLE = "Расходы за день";
-var FREE_DAILY_CASH_DATA_TITLE = "Свободных наличных на день:"
-var CURRENCY_SUFFIX = " zł";
-var EMPTY_STRING = "";
+
+const PLANNED_PAYMENT_WARNING_TITLE = "Необходимо проверить список транзакций!";
+const DAILY_EXPENSES_DATA_TITLE = "Расходы за день";
+const FREE_DAILY_CASH_DATA_TITLE = "Свободных наличных на день:"
+const CURRENCY_SUFFIX = " zł";
+const EMPTY_STRING = "";
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # home-money
 Google App Scripts to power up Google Spreadsheet application for home finances
+# Prerequisites
+npm install --save @types/google-apps-script - for VS Code intellisense
